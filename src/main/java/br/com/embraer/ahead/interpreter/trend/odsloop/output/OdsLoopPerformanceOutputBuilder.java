c6b60b65ce0bafcfa549c6bcb894aad4fb6b58c4
rafaelocctopusaicom <rafaelocctopusaicom@users.noreply.github.com>
2026-06-29 21:59:07 -0300

    feat: cria interpreter-trend-ods-loop-performance (modulo isolado)

--- src/main/java/br/com/embraer/ahead/interpreter/trend/odsloop/output/OdsLoopPerformanceOutputBuilder.java
+++ src/main/java/br/com/embraer/ahead/interpreter/trend/odsloop/output/OdsLoopPerformanceOutputBuilder.java
@@ -1,21 +1,9 @@
-
 package br.com.embraer.ahead.interpreter.trend.odsloop.output;
-
 import br.com.embraer.ahead.interpreter.api.output.OutputBuilder;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-
 public class OdsLoopPerformanceOutputBuilder implements OutputBuilder {
-
     public static final String TOPIC = "system-trends";
-
-    @Override
-    public String topicName() {
-        return TOPIC;
-    }
-
-    @Override
-    public Dataset<Row> build(Dataset<Row> dataframe) {
-        return dataframe;
-    }
+    @Override public String topicName() { return TOPIC; }
+    @Override public Dataset<Row> build(Dataset<Row> dataframe) { return dataframe; }
 }
